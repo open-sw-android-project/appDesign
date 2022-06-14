0317dbd4404d93a8c6b045c2a6a5d4c499b64c1b
TaeHyun Park <pthkuku99@gmail.com>
2022-06-14 18:29:05 +0900

    redesign button, set onclick On dynamic button

--- Attendance/app/src/main/java/com/cookandroid/attendance/MainActivity.java
+++ Attendance/app/src/main/java/com/cookandroid/attendance/MainActivity.java
@@ -4,27 +4,29 @@ import androidx.annotation.IdRes;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     int lectureNum = 0;         //수강중인 강의 개수
     LinearLayout lectureList;   //강의가 추가 될 레이아웃
     TextView tvLecture;         //수강목록 [총 n건]
-
-    ArrayList<Button> lecBtnList = new ArrayList<Button>(); // 강의버튼 저장할 배열리스트
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         addLecture("컴퓨터네트워크");
         addLecture("창업기획");
 
-        tvLecture.setText("     수강목록 [총 " + lectureNum + "건]" );
+        tvLecture.setText("     수강목록 [총 " + lectureNum + "건]" ); // 수강목록 텍스트 set
 
     }
 
@@ -54,16 +56,26 @@ public class MainActivity extends AppCompatActivity {
         lecture.setMinLines(2);
         lecture.setGravity(Gravity.LEFT);
         lecture.setTextSize(20);
-        lecture.setTextColor(Color.BLACK);
+        lecture.setTextColor(Color.WHITE);
         lecture.setTypeface(null, Typeface.BOLD);
-        lecture.setId(0);
+        lecture.setId(lectureNum++);
+        lecture.setPadding(10, 10 ,10 , 10);
+        lecture.setBackground(getResources().getDrawable(R.drawable.btn_2));
         LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         param.leftMargin = 30;
+        param.bottomMargin = 10;
         lecture.setLayoutParams(param);
 
+        lecture.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Log.d("Btn: ", LectureName );
+            }
+
+        });
+
         lectureList.addView(lecture);  // 레이아웃에 강의 추가
-        lectureNum++; // 강의 개수 증가
-        lecBtnList.add(lecture);
+         // 강의 개수 증가
     }
 
 }
